b6a954b27171328fa772d8ee71ff6e05a7b4f451
ysmin <ysmin@ntels.com>
2025-02-14 15:35:07 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "test"));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  return <h1>🔥 Firebase 연결 완료!</h1>;
+}
+
+export default App;
